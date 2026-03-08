51cae7e7756c649b4aa2bc42e64963f732b56f2d
Parmeshwar-razz <parmeshwarrazz6221@gmail.com>
2026-03-08 14:07:21 +0530

    resume update

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -60,6 +60,7 @@ export function Hero() {
                                 </Button>
                             </a>
                         )}
+                        console.log(resumeUrl)
                     </div>
                 </div>
 
